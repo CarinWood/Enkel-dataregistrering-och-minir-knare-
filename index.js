6b6b26d9b2ac5452aa0165dff54af69de3d32843
CarinWood <carin.wood.85@gmail.com>
2024-01-11 10:17:45 +0100

    exercises finished

---
+++ index.js
@@ -0,0 +1,27 @@
+//1
+
+let userName = prompt('Enter your name:')
+console.log(`Welcome ${userName}!`)
+
+//2
+
+const birthyear = prompt("Enter your birth year (4 digits):")
+
+//3
+
+const userAge = 2024-birthyear
+console.log(`You are ${userAge} years old.`)
+
+
+//4
+
+const num01 = prompt("Enter a number:")
+const num02 = prompt("Enter another number:")
+
+//5
+console.log(parseInt(num01) + parseInt(num02))
+console.log(parseInt(num01) - parseInt(num02))
+console.log(parseInt(num01) * parseInt(num02))
+
+//6
+alert('The calculation is now finished! Open console to see the results')
